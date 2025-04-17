7bd66b43d3ff78827c5c5999f595bc22d679c89e
abdul-qadir92 <abdulqadir.k@browserstack.com>
2025-04-17 15:43:31 +0530

    text logs

--- ios/bstack_sample.py
+++ ios/bstack_sample.py
@@ -11,16 +11,27 @@ from selenium.webdriver.common.by import By
 class TestSample:
 
     def test_example(self):
+
+        time.sleep(15)
         text_button = WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Text Button"))
         )
+        self.driver.execute_script(
+            'browserstack_executor: {"action": "annotate", "arguments": {"data":"Click on Text Button", "level": "info"}}')
+
         text_button.click()
         text_input = WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Text Input"))
         )
+        self.driver.execute_script(
+            'browserstack_executor: {"action": "annotate", "arguments": {"data":"Enter Text", "level": "debug"}}')
+
         text_input.send_keys("hello@browserstack.com"+"\n")
         time.sleep(5)
         text_output = WebDriverWait(self.driver, 30).until(
             EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Text Output"))
         )
+        self.driver.execute_script(
+            'browserstack_executor: {"action": "annotate", "arguments": {"data":"Confirm the Result", "level": "warn"}}')
+
         assert text_output!=None and text_output.text=="hello@browserstack.com"
